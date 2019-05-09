864824df4aaf87cc0c20aa9ad2f51b3e1e029c9f
frerly <frerly@localhost>
2019-05-09 00:28:06 -0500

    Muevo el proyecto a otra carpeta para

---
+++ bobcito-client-fe/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './assets/index.css';
+import App from './app/App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+//import * as serviceWorker from './serviceWorker';
+
+/*
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}*/
+
+ReactDOM.render(<Router><App /></Router>,document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
